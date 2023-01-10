255ba85724ee9ef56d585cc8d401f14ffe3de70c
matomugo60 <matomugo60@gmail.com>
2023-01-10 15:54:28 +0300

    Complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+const username = "Liza";
+const city = "New York";
+
+
 function Home() {
   return (
     <div id="home">
@@ -9,3 +13,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
